8ffd8a1ad25283312f0335ae95a6471b1eb33e75
rodrigo_baroni <rodrigobaroni15@gmail.com>
2021-10-09 08:31:43 -0300

    First Commit

---
+++ MeuPod/Classes/Logger.swift
@@ -0,0 +1,24 @@
+//
+//  Logger.swift
+//  MeuPod
+//
+//  Created by Rodrigo Baroni on 09/10/21.
+//
+
+import Foundation
+
+public class Logger {
+    
+    public init() {
+        
+    }
+    
+    public func printLog() {
+        print("Back Front Academy")
+    }
+    
+    func privateLog() {
+        print("Log private")
+    }
+    
+}
